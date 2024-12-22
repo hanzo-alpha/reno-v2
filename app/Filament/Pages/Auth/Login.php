c34d4f40483f5e602998331045b23ddde727d76a
Hanzo Alpha <hansen.makangiras@hotmail.com>
2024-12-22 18:22:31 +0800

    Tambah file aset logo dan ikon baru
    Menambahkan berbagai file aset baru termasuk logo dengan berbagai varian format (JPEG, PNG, SVG, PDF) dan warna untuk keperluan antarmuka dan branding. File-file ini mencakup desain logo Dinsos serta ikon resolusi tinggi dan transparan. Semua perubahan ditujukan untuk mendukung kebutuhan desain yang lebih fleksibel.
    
    Signed-off-by: Hanzo Alpha <hansen.makangiras@hotmail.com>

--- app/Filament/Pages/Auth/Login.php
+++ app/Filament/Pages/Auth/Login.php
@@ -2,10 +2,55 @@
 
 namespace App\Filament\Pages\Auth;
 
+use Filament\Forms\Components\Component;
+use Filament\Forms\Components\TextInput;
 use Filament\Pages\Auth\Login as BasePage;
+use Illuminate\Validation\ValidationException;
 
 class Login extends BasePage
 {
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getLoginFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getRememberFormComponent(),
+                    ])
+                    ->statePath('data'),
+            ),
+        ];
+    }
+
+    protected function getLoginFormComponent(): Component
+    {
+        return TextInput::make('login')
+            ->label(__('Username / Email'))
+            ->required()
+            ->autocomplete()
+            ->autofocus()
+            ->default(fn() => app()->isLocal() ? config('app.default_user.email') : null)
+            ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function getCredentialsFromFormData(array $data): array
+    {
+        $login_type = filter_var($data['login'], FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+        return [
+            $login_type => $data['login'],
+            'password' => $data['password'],
+        ];
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.login' => __('filament-panels::pages/auth/login.messages.failed'),
+        ]);
+    }
+
     public function mount(): void
     {
         parent::mount();
